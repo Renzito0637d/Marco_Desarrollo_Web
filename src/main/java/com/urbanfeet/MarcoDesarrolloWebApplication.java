a1b772790713f268e6a6f641101818c90d154370
renzito0637d <renzito0637d@gmail.com>
2025-05-03 12:16:33 -0500

    Proyecto generado

---
+++ src/main/java/com/urbanfeet/MarcoDesarrolloWebApplication.java
@@ -0,0 +1,13 @@
+package com.urbanfeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcoDesarrolloWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcoDesarrolloWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urbanfeet/MarcoDesarrolloWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbanfeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcoDesarrolloWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
