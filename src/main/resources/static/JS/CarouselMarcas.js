3291822cc2e631579b140600f8d72f8875b7c05a
renzito0637d <renzito0637d@gmail.com>
2025-05-08 22:53:02 -0500

    Add carousel for brand logos in CarouselMarcas.js

---
+++ src/main/resources/static/JS/CarouselMarcas.js
@@ -0,0 +1,51 @@
+function createCarouselMarcas() {
+
+    const headerHTML = `
+    <section class="marcas-section">
+      <h2 data-aos="zoom-in-down" id="productosS" class="marcas-h2">Marcas que te ofrecemos</h2>
+      <div data-aos="zoom-in-down" class="carr">
+        <div class="slide-track">
+          <div class="slide">
+            <img src="../static/IMG/i-run.png" alt="">
+          </div>
+          <div class="slide">
+            <img src="../static/IMG/nike.png" alt="">
+          </div>
+          <div class="slide">
+            <img src="../static/IMG/atletix.png" alt="">
+          </div>
+          <div class="slide">
+            <img src="../static/IMG/newAtletic.png" alt="">
+          </div>
+          <div class="slide">
+            <img src="../static/IMG/adidas.png" alt="">
+          </div>
+          <div class="slide">
+            <img src="../static/IMG/punto.png" alt="">
+          </div>
+
+          <div class="slide">
+            <img src="../static/IMG/i-run.png" alt="">
+          </div>
+          <div class="slide">
+            <img src="../static/IMG/nike.png" alt="">
+          </div>
+          <div class="slide">
+            <img src="../static/IMG/atletix.png" alt="">
+          </div>
+          <div class="slide">
+            <img src="../static/IMG/newAtletic.png" alt="">
+          </div>
+          <div class="slide">
+            <img src="../static/IMG/adidas.png" alt="">
+          </div>
+          <div class="slide">
+            <img src="../static/IMG/punto.png" alt="">
+          </div>
+      </div>
+
+    </section>
+    `
+
+    document.body.insertAdjacentHTML('beforeend', headerHTML);
+}
