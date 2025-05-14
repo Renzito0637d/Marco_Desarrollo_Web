784ab7fd8c116c09666893e4b8896c61bb75f1cb
SherooRV <U22312945@utp.edu.pe>
2025-05-13 23:30:07 -0500

    Redes

--- src/main/resources/static/JS/Header.js
+++ src/main/resources/static/JS/Header.js
@@ -18,7 +18,7 @@ function createHeader() {
             <li><a href="#" class="nav-link px-2 text-white">Catalogo</a></li>
             <li><a href="#" class="nav-link px-2 text-white">Nosotros</a></li>
             <li><a href="#" class="nav-link px-2 text-white">Contactanos</a></li>
-            <li><a href="#" class="nav-link px-2 text-white">Redes</a></li>
+            <li><a href="../templates/Redes.html" class="nav-link px-2 text-white">Redes</a></li>
             <li><a href="#" class="nav-link px-2 text-white">Mis pedidos</a></li>
           </ul>
 
