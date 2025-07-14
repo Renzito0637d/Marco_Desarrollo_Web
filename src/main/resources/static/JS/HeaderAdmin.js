247aedc1e34c8c998450a49eb8ba6a1e54e247eb
angxl10t <luang1007@gmail.com>
2025-07-14 01:27:13 -0500

    Update HeaderAdmin.js

--- src/main/resources/static/JS/HeaderAdmin.js
+++ src/main/resources/static/JS/HeaderAdmin.js
@@ -19,16 +19,16 @@ function createHeader() {
         <div class="collapse navbar-collapse" id="navbarAdmin">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-            <a class="nav-link active" href="Pedidos.html">Pedidos</a>
+            <a class="nav-link active" href="/admin/pedidos">Pedidos</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="Productos.html">Productos</a>
+            <a class="nav-link" href="/admin/catalogo">Productos</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="Usuarios.html">Usuarios</a>
+            <a class="nav-link" href="/admin/usuarios">Usuarios</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="Almacen.html">Almacen</a>
+            <a class="nav-link" href="/admin/almacen">Almacen</a>
             </li>
         </ul>
 
