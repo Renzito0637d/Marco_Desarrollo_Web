2c897bc6fc84c45b617497bd658c022d9fc0799d
renzito0637d <renzito0637d@gmail.com>
2025-07-20 00:02:29 -0500

    Repositorios JPA impl carrito

--- src/main/java/com/urbanfeet/Repository/CarritoRepository.java
+++ src/main/java/com/urbanfeet/Repository/CarritoRepository.java
@@ -1,11 +1,14 @@
 package com.urbanfeet.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.urbanfeet.Entity.Carrito;
+import com.urbanfeet.Entity.Usuario;
 
 @Repository
 public interface CarritoRepository extends JpaRepository<Carrito, Integer> {
-    
+    Optional<Carrito> findByUsuario(Usuario usuario);
 }
--- src/main/java/com/urbanfeet/Repository/ProductoVariacionRepository.java
+++ src/main/java/com/urbanfeet/Repository/ProductoVariacionRepository.java
@@ -2,11 +2,16 @@ package com.urbanfeet.Repository;
 
 import com.urbanfeet.Entity.ProductoVariacion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface ProductoVariacionRepository extends JpaRepository<ProductoVariacion, Integer> {
     List<ProductoVariacion> findByProductoId(int productoId);
 
     List<ProductoVariacion> findByStockGreaterThan(int minStock);
+
+    Optional<ProductoVariacion> findByProductoIdAndColor(Integer productoId, String color);
 }
